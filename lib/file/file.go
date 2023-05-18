2de0f46991d7523f0a3c1e3683f34502044ef7fd
wjwzero <13602573297@163.com>
2023-05-18 09:12:12 +0800

    1. file DynamicConfig

--- lib/file/file.go
+++ lib/file/file.go
@@ -6,6 +6,7 @@ import (
 	"github.com/astaxie/beego/logs"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -16,25 +17,31 @@ import (
 
 func NewJsonDb(runPath string) *JsonDb {
 	return &JsonDb{
-		RunPath:        runPath,
-		TaskFilePath:   filepath.Join(runPath, "conf", "tasks.json"),
-		HostFilePath:   filepath.Join(runPath, "conf", "hosts.json"),
-		ClientFilePath: filepath.Join(runPath, "conf", "clients.json"),
+		RunPath:               runPath,
+		TaskFilePath:          filepath.Join(runPath, "conf", "tasks.json"),
+		HostFilePath:          filepath.Join(runPath, "conf", "hosts.json"),
+		ClientFilePath:        filepath.Join(runPath, "conf", "clients.json"),
+		TunnelTypesFilePath:   filepath.Join(runPath, "conf", "tunnelTypes.json"),
+		DynamicConfigFilePath: filepath.Join(runPath, "conf", "dynamicConfig.json"),
 	}
 }
 
 type JsonDb struct {
-	Tasks            sync.Map
-	Hosts            sync.Map
-	HostsTmp         sync.Map
-	Clients          sync.Map
-	RunPath          string
-	ClientIncreaseId int32  //client increased id
-	TaskIncreaseId   int32  //task increased id
-	HostIncreaseId   int32  //host increased id
-	TaskFilePath     string //task file path
-	HostFilePath     string //host file path
-	ClientFilePath   string //client file path
+	Tasks                 sync.Map
+	Hosts                 sync.Map
+	HostsTmp              sync.Map
+	Clients               sync.Map
+	TunnelTypes           sync.Map
+	DynamicConfig         sync.Map
+	RunPath               string
+	ClientIncreaseId      int32  //client increased id
+	TaskIncreaseId        int32  //task increased id
+	HostIncreaseId        int32  //host increased id
+	TaskFilePath          string //task file path
+	HostFilePath          string //host file path
+	ClientFilePath        string //client file path
+	TunnelTypesFilePath   string //tunnelTypes file path
+	DynamicConfigFilePath string //tunnelTypes file path
 }
 
 func (s *JsonDb) LoadTaskFromJsonFile() {
@@ -60,10 +67,16 @@ func (s *JsonDb) LoadClientFromJsonFile() {
 		if json.Unmarshal([]byte(v), &post) != nil {
 			return
 		}
-		if post.RateLimit > 0 {
+		rateFlag := common.GetRateFlag()
+		if post.RateLimit > 0 && rateFlag {
 			post.Rate = rate.NewRate(int64(post.RateLimit * 1024))
 		} else {
-			post.Rate = rate.NewRate(int64(2 << 23))
+			var rateMaxErr error
+			var rateLimit int64
+			if rateLimit, rateMaxErr = s.GetCommonRateLimitMax(); rateMaxErr != nil {
+				logs.Error("获取动态配置失败 GetCommonRateLimitMax 默认 %s kb", rateLimit, rateMaxErr.Error())
+			}
+			post.Rate = rate.NewRate(int64(rateLimit * 1024))
 		}
 		post.Rate.Start()
 		post.NowConn = 0
@@ -88,6 +101,27 @@ func (s *JsonDb) LoadHostFromJsonFile() {
 		if post.Id > int(s.HostIncreaseId) {
 			s.HostIncreaseId = int32(post.Id)
 		}
+	})
+}
+
+func (s *JsonDb) LoadTunnelTypesProductRelationFromJsonFile() {
+	loadSyncMapFromFile(s.TunnelTypesFilePath, func(v string) {
+		tunnelTypes := new(TunnelTypesProductRelation)
+		if json.Unmarshal([]byte(v), &tunnelTypes) != nil {
+			return
+		}
+		s.TunnelTypes.Store(tunnelTypes.ProductKey, tunnelTypes)
+	})
+}
+
+func (s *JsonDb) LoadDynamicConfigFromJsonFile() {
+	loadSyncMapFromFile(s.DynamicConfigFilePath, func(v string) {
+		dynamicConfig := new(DynamicConfig)
+		if json.Unmarshal([]byte(v), &dynamicConfig) != nil {
+			return
+		}
+		s.DynamicConfig.Store(dynamicConfig.Key, dynamicConfig)
+		logs.Info("=============初始化 动态配置 Key:%s, Value:%s =================", dynamicConfig.Key, dynamicConfig.Value)
 	})
 }
 
@@ -122,6 +156,22 @@ func (s *JsonDb) StoreClientsToJsonFile() {
 	clientLock.Lock()
 	storeSyncMapToFile(s.Clients, s.ClientFilePath)
 	clientLock.Unlock()
+}
+
+var tunnelTypeLock sync.Mutex
+
+func (s *JsonDb) StoreTunnelTypeToJsonFile() {
+	tunnelTypeLock.Lock()
+	storeSyncMapToFile(s.TunnelTypes, s.TunnelTypesFilePath)
+	tunnelTypeLock.Unlock()
+}
+
+var dynamicConfigLock sync.Mutex
+
+func (s *JsonDb) StoreDynamicConfigToJsonFile() {
+	dynamicConfigLock.Lock()
+	storeSyncMapToFile(s.DynamicConfig, s.DynamicConfigFilePath)
+	dynamicConfigLock.Unlock()
 }
 
 func (s *JsonDb) GetClientId() int32 {
@@ -174,6 +224,9 @@ func storeSyncMapToFile(m sync.Map, filePath string) {
 				return true
 			}
 			b, err = json.Marshal(obj)
+		case *TunnelTypesProductRelation:
+			obj := value.(*TunnelTypesProductRelation)
+			b, err = json.Marshal(obj)
 		default:
 			return true
 		}
@@ -198,4 +251,34 @@ func storeSyncMapToFile(m sync.Map, filePath string) {
 		logs.Error(err, "store to file err, data will lost")
 	}
 	// replace the file, maybe provides atomic operation
+}
+
+func (s *JsonDb) GetDynamicConfig(key string) (c *DynamicConfig, err error) {
+	if v, ok := s.DynamicConfig.Load(key); ok {
+		c = v.(*DynamicConfig)
+		return
+	}
+	err = errors.New("未找到配置")
+	return
+}
+
+func (s *JsonDb) CheckRateLimitFlag() (rateLimit bool, err error) {
+	var d *DynamicConfig
+	if d, err = s.GetDynamicConfig("RateLimit"); err == nil {
+		rateLimitStr := d.Value
+		rateLimit, err = strconv.ParseBool(rateLimitStr)
+		return
+	}
+	return
+}
+
+func (s *JsonDb) GetCommonRateLimitMax() (rateLimitMax int64, err error) {
+	var d *DynamicConfig
+	rateLimitMax = 50000
+	if d, err = s.GetDynamicConfig("RateLimitMax"); err == nil {
+		rateLimitMaxStr := d.Value
+		rateLimitMax, err = strconv.ParseInt(rateLimitMaxStr, 10, 64)
+		return
+	}
+	return
 }
