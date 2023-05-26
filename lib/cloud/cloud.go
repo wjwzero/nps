c2c7e21ced49d579ba351bff313f89dc16730fb0
wjwzero <13602573297@163.com>
2023-05-26 09:10:05 +0800

    1. nps db log update

--- lib/cloud/cloud.go
+++ lib/cloud/cloud.go
@@ -29,11 +29,11 @@ retry:
 	if err != nil {
 		retryNum++
 		if retryNum < 3 {
-			retryDuration = 10
+			retryDuration = 100 + rand.Intn(30)
 		} else if retryNum >= 3 {
-			retryDuration = 50 + rand.Intn(10)
+			retryDuration = 150 + rand.Intn(30)
 		} else if retryNum >= 5 {
-			retryDuration = 170 + rand.Intn(10)
+			retryDuration = 300 + rand.Intn(30)
 		} else {
 			retryDuration = 9*60 + rand.Intn(60)
 		}
@@ -47,7 +47,7 @@ retry:
 // getNpsNodeExternalIp 从云平台获得节点外部IP地址 client 使用
 func getNpsNodeExternalIp(cloudAddr string, deviceKey string) (ip string, err error) {
 	var data CommonData
-	resp, err := http.Get(cloudAddr + "/xxxDispatch/ip?deviceKey=" + deviceKey)
+	resp, err := http.Get(cloudAddr + "/nps/ip?deviceKey=" + deviceKey)
 	if err != nil {
 		return "", err
 	}
@@ -66,7 +66,7 @@ func CheckPassword(cloudAddr string, vKey string, password string) (res bool, er
 	sign := crypt.CreateSign([]string{vKey, password})
 	var data CommonData
 	cloudAddr = beego.AppConfig.String("cloudAddr")
-	resp, err := http.Get(cloudAddr + "/xxxUser/verify" + password + "&deviceKey=" + vKey + "&authString=" + sign)
+	resp, err := http.Get(cloudAddr + "/judgeNasPassword?password=" + password + "&deviceKey=" + vKey + "&authString=" + sign)
 	if err != nil {
 		return false, err
 	}
@@ -84,22 +84,25 @@ func CheckPassword(cloudAddr string, vKey string, password string) (res bool, er
 }
 
 // CheckDeviceKey 从云平台验证vKey dk server 使用
-func CheckDeviceKey(cloudAddr string, deviceKey string) (res bool, err error) {
+func CheckDeviceKey(cloudAddr string, deviceKey string) (res bool, productKey string, err error) {
 	sign := crypt.CreateSign([]string{deviceKey})
 	var data CommonData
-	resp, err := http.Get(cloudAddr + "/xxxDevice/verifyDeviceKey" + deviceKey + "&authString=" + sign)
+	resp, err := http.Get(cloudAddr + "/judgeDeviceExist?deviceKey=" + deviceKey + "&authString=" + sign)
 	if err != nil {
-		return false, err
+		return false, "", err
 	}
 	defer resp.Body.Close()
 	content, _ := ioutil.ReadAll(resp.Body)
 	err = json.Unmarshal(content, &data)
 	if err != nil {
-		return false, err
+		return false, "", err
 	}
 	if data.Code != 200 {
-		return false, errors.New(data.Msg)
+		return false, "", errors.New(data.Msg)
 	}
 	res = data.Data["judge"].(bool)
+	if data.Data["productKey"] != nil {
+		productKey = data.Data["productKey"].(string)
+	}
 	return
 }
