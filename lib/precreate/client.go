0acfbd6fad24c7b7bfb7273b6d4734b695baa11f
wjwzero <13602573297@163.com>
2023-07-07 08:52:26 +0800

    1. client.go push

--- lib/precreate/client.go
+++ lib/precreate/client.go
@@ -1,39 +1,48 @@
 package preclient
 
 import (
+	"ehang.io/nps/db"
 	"ehang.io/nps/lib/file"
+	"ehang.io/nps/models"
 	"ehang.io/nps/server/tool"
 	"errors"
 	"fmt"
 	"github.com/astaxie/beego"
 )
 
-func P2pClient(clientId int, deviceKey string, password string) (t *file.Tunnel, err error) {
+var (
+	ClientDao db.ClientDao
+	TaskDao   db.TaskDao
+	HostDao   db.HostDao
+)
+
+func P2pClient(clientId int, deviceKey string, password string) (t *models.NpsClientTaskInfo, err error) {
 	// P2P 端代理目标端口号
 	p2pListenPort := beego.AppConfig.String("p2p_listen_port")
-	t = &file.Tunnel{
-		Port:      0,
-		ServerIp:  "",
-		Mode:      "p2p",
-		Target:    &file.Target{TargetStr: p2pListenPort, LocalProxy: false},
-		Id:        int(file.GetDb().JsonDb.GetTaskId()),
-		Status:    true,
-		Remark:    deviceKey,
-		Password:  password,
-		LocalPath: "",
-		StripPre:  "",
-		Flow:      &file.Flow{},
+	t = &models.NpsClientTaskInfo{
+		RunStatus:    true,
+		Port:         0,
+		ServerIp:     "",
+		Mode:         "p2p",
+		TargetStr:    p2pListenPort,
+		IsLocalProxy: false,
+		Status:       true,
+		Remark:       deviceKey,
+		Password:     password,
+		LocalPath:    "",
+		StripPre:     "",
+		Flow:         &file.Flow{},
 	}
 	if !tool.TestServerPort(t.Port, t.Mode) {
 		return nil, errors.New("the port cannot be opened because it may has been occupied or is no longer allowed")
 	}
-	if t.Client, err = file.GetDb().GetClient(clientId); err != nil {
+	if t.Client, err = ClientDao.GetClientInfo(clientId); err != nil {
 		return nil, err
 	}
-	if t.Client.MaxTunnelNum != 0 && t.Client.GetTunnelNum() >= t.Client.MaxTunnelNum {
+	if t.Client.MaxChannelNum != 0 && TaskDao.GetTunnelNum(t.Client.Id) >= t.Client.MaxChannelNum {
 		return nil, err
 	}
-	if err := file.GetDb().NewTask(t); err != nil {
+	if err := TaskDao.NewTask(t); err != nil {
 		return nil, errors.New(fmt.Sprintf("the number of tunnels exceeds the limit %s", err.Error()))
 	}
 	return
