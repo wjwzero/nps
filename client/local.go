6bb61211787527371394502ff23b25eca4dec402
wjwzero <13602573297@163.com>
2023-09-11 09:37:53 +0800

    1. local.go push

--- client/local.go
+++ client/local.go
@@ -3,7 +3,9 @@ package client
 import (
 	"ehang.io/nps-mux"
 	"ehang.io/nps/lib/crypt"
+	"ehang.io/nps/models"
 	"errors"
+	"math/rand"
 	"net"
 	"net/http"
 	"runtime"
@@ -21,20 +23,21 @@ import (
 )
 
 var (
-	LocalServer   []*net.TCPListener
-	udpConn       net.Conn
-	muxSession    *nps_mux.Mux
-	fileServer    []*http.Server
-	p2pNetBridge  *p2pBridge
-	lock          sync.RWMutex
-	udpConnStatus bool
-	connStatus    string
+	LocalServer     []*net.TCPListener
+	udpConn         net.Conn
+	muxSession      *nps_mux.Mux
+	fileServer      []*http.Server
+	p2pNetBridge    *p2pBridge
+	lock            sync.RWMutex
+	udpConnStatus   bool
+	connStatus      string
+	tunneltypesTemp string
 )
 
 type p2pBridge struct {
 }
 
-func (p2pBridge *p2pBridge) SendLinkInfo(clientId int, link *conn.Link, t *file.Tunnel) (target net.Conn, err error) {
+func (p2pBridge *p2pBridge) SendLinkInfo(clientId int, link *conn.Link, t *models.NpsClientTaskInfo) (target net.Conn, err error) {
 	for i := 0; muxSession == nil; i++ {
 		if i >= 20 {
 			err = errors.New("p2pBridge:too many times to get muxSession")
@@ -64,7 +67,7 @@ func CloseLocalServer() {
 	}
 }
 
-func startLocalFileServer(config *config.CommonConfig, t *file.Tunnel, vkey string) {
+func startLocalFileServer(config *config.CommonConfig, t *models.NpsClientTaskInfo, vkey string) {
 	remoteConn, err := NewConn(config.Tp, vkey, config.Server, common.WORK_FILE, config.ProxyUrl)
 	if err != nil {
 		logs.Error("Local connection server failed ", err.Error())
@@ -80,25 +83,24 @@ func startLocalFileServer(config *config.CommonConfig, t *file.Tunnel, vkey stri
 }
 
 func StartLocalServer(l *config.LocalServer, config *config.CommonConfig) error {
-	if l.Type != "secret" {
+	var tunnelTypes string
+	var typeErr error
+	// 获取隧道类型
+	if tunnelTypes, typeErr = getTunnelTypes(config, l); typeErr != nil {
+		tunnelTypes = common.DEFULT_TUNNEL_TYPE
+		logs.Warn("未从平台获取到 tunnelTypes 采用默认 %s", tunnelTypes)
+	}
+	logs.Info("从平台获取到 tunnelTypes 采用 %s", tunnelTypes)
+	if strings.Contains(tunnelTypes, common.P2P_TUNNEL_TYPE) {
+		logs.Info("TunnelTypes includes P2P, which is used to attempt UDP interaction.")
 		go handleUdpMonitor(config, l)
 	}
-	task := &file.Tunnel{
+	task := &models.NpsClientTaskInfo{
 		Port:     l.Port,
 		ServerIp: "0.0.0.0",
 		Status:   true,
-		Client: &file.Client{
-			Cnf: &file.Config{
-				U:        "",
-				P:        "",
-				Compress: config.Client.Cnf.Compress,
-			},
-			Status:    true,
-			RateLimit: 0,
-			Flow:      &file.Flow{},
-		},
-		Flow:   &file.Flow{},
-		Target: &file.Target{},
+		Client:   &models.NpsClientListInfo{},
+		Flow:     &file.Flow{},
 	}
 	switch l.Type {
 	case "p2ps":
@@ -115,27 +117,59 @@ func StartLocalServer(l *config.LocalServer, config *config.CommonConfig) error 
 		}
 		LocalServer = append(LocalServer, listener)
 		logs.Info("successful start-up of local tcp monitoring, port", l.Port)
+	retry:
 		conn.Accept(listener, func(c net.Conn) {
-			logs.Trace("new %s connection", l.Type)
-			// 进行局域网
-			if err = handleRemoteLocalAddr(c, config, l); err != nil {
-				logs.Info("LAN connect error %s", err.Error())
-				if l.Type == "secret" {
-					handleSecret(c, config, l)
-				} else if l.Type == "p2p" {
-					handleP2PVisitor(c, config, l)
-				}
+			// 获取隧道类型
+			logs.Trace("new %s connection", tunnelTypes)
+			var p2perr error
+			// 隧道类型包含 P2P
+			if strings.Contains(tunnelTypes, common.P2P_TUNNEL_TYPE) {
+				l.ConnStatus = common.P2P_TUNNEL_TYPE
+				p2perr = handleP2PVisitor(c, config, l)
+			}
+			// 隧道包含Realy 或 隧道类型包含Relay 且 P2P失败
+			if strings.Contains(tunnelTypes, common.RELAY_TUNNEL_TYPE) || (strings.Contains(tunnelTypes, common.RELAY_TUNNEL_TYPE) && p2perr != nil) {
+				l.ConnStatus = common.RELAY_TUNNEL_TYPE
+				handleSecret(c, config, l)
 			}
 			logs.Info("now connect Type ->>>>>>:", l.ConnStatus)
 		})
+		goto retry
 	}
 	return nil
+}
+
+func switchTunnel(c net.Conn, tunnelTypeArr []string, l *config.LocalServer, config *config.CommonConfig) bool {
+	for _, tunnelType := range tunnelTypeArr {
+		switch tunnelType {
+		case common.LAN_TUNNEL_TYPE:
+			l.ConnStatus = common.LAN_TUNNEL_TYPE
+			logs.Info("start connect Type :", l.ConnStatus)
+			if err := handleRemoteLocalAddr(c, config, l); err != nil {
+				logs.Info("end connect Type :%s e: %s", l.ConnStatus, err.Error())
+				return true
+			}
+		case common.P2P_TUNNEL_TYPE:
+			l.ConnStatus = common.P2P_TUNNEL_TYPE
+			logs.Info("start connect Type :", l.ConnStatus)
+			if err := handleP2PVisitor(c, config, l); err != nil {
+				logs.Info("end connect Type :%s e: %s", l.ConnStatus, err.Error())
+				return true
+			}
+		case common.RELAY_TUNNEL_TYPE:
+			l.ConnStatus = common.RELAY_TUNNEL_TYPE
+			logs.Info("start connect Type :", l.ConnStatus)
+			handleSecret(c, config, l)
+		}
+	}
+	return false
 }
 
 func handleUdpMonitor(config *config.CommonConfig, l *config.LocalServer) {
 	ticker := time.NewTicker(time.Second * 1)
 	defer ticker.Stop()
 	for {
+		var retryNum = 0
 		select {
 		case <-ticker.C:
 			if !udpConnStatus {
@@ -145,14 +179,25 @@ func handleUdpMonitor(config *config.CommonConfig, l *config.LocalServer) {
 					logs.Error(err)
 					return
 				}
+				var retryDuration = 10
 				for i := 0; i < 10; i++ {
-					logs.Notice("try to connect to the server after 10s ", i+1)
+					retryNum++
+					if retryNum < 3 {
+						retryDuration = 10 + rand.Intn(5)
+					} else if retryNum >= 3 && retryNum < 5 {
+						retryDuration = 15 + rand.Intn(5)
+					} else if retryNum >= 5 && retryNum < 10 {
+						retryDuration = 30 + rand.Intn(5)
+					} else {
+						retryDuration = 9*6 + rand.Intn(6)
+					}
+					logs.Notice("try to connect to the server after %d s, time %d ", retryDuration, i+1)
 					newUdpConn(tmpConn.LocalAddr().String(), config, l)
 					if udpConn != nil {
 						udpConnStatus = true
 						break
 					}
-					time.Sleep(time.Duration(10) * time.Second)
+					time.Sleep(time.Duration(retryDuration) * time.Second)
 				}
 			}
 		}
@@ -160,13 +205,13 @@ func handleUdpMonitor(config *config.CommonConfig, l *config.LocalServer) {
 }
 
 func handleRemoteLocalAddr(localTcpConn net.Conn, config *config.CommonConfig, l *config.LocalServer) (err error) {
-	err, localIp := common.GetIntranetIp()
+	tmpConn, err := common.GetLocalUdpAddr()
 	if err != nil {
 		logs.Error(err)
 		return
 	}
-	l.ConnStatus = "LAN"
-	err = newRemoteLocalConn(localTcpConn, localIp, config, l)
+	//l.ConnStatus = "LAN"
+	err = newRemoteLocalConn(localTcpConn, tmpConn.LocalAddr().String(), config, l)
 	return
 }
 
@@ -184,25 +229,26 @@ func handleSecret(localTcpConn net.Conn, config *config.CommonConfig, l *config.
 	conn.CopyWaitGroup(remoteConn.Conn, localTcpConn, false, false, nil, nil, false, nil)
 }
 
-func handleP2PVisitor(localTcpConn net.Conn, config *config.CommonConfig, l *config.LocalServer) {
+func handleP2PVisitor(localTcpConn net.Conn, config *config.CommonConfig, l *config.LocalServer) (err error) {
 	if udpConn == nil {
 		logs.Notice("new conn, P2P can not penetrate successfully, traffic will be transferred through the server")
-		l.ConnStatus = "relay"
-		logs.Info("now connect Type :", l.ConnStatus)
-		handleSecret(localTcpConn, config, l)
+		//l.ConnStatus = "relay"
+		err = errors.New("P2P 未打通, 如配置 relay 将尝试relay")
+		logs.Warn(err.Error())
+		//handleSecret(localTcpConn, config, l)
 		return
 	}
 	logs.Trace("start trying to connect with the server")
 	//TODO just support compress now because there is not tls file in client packages
-	link := conn.NewLink(common.CONN_TCP, l.Target, false, config.Client.Cnf.Compress, localTcpConn.LocalAddr().String(), false)
-	if target, err := p2pNetBridge.SendLinkInfo(0, link, nil); err != nil {
-		logs.Error(err)
+	link := conn.NewLink(common.CONN_TCP, l.Target, false, config.Client.IsCompress, localTcpConn.LocalAddr().String(), false)
+	if target, bErr := p2pNetBridge.SendLinkInfo(0, link, nil); bErr != nil {
+		logs.Error(bErr)
 		udpConnStatus = false
-		return
+		return bErr
 	} else {
-		l.ConnStatus = "P2P"
-		logs.Info("now connect Type :", l.ConnStatus)
-		conn.CopyWaitGroup(target, localTcpConn, false, config.Client.Cnf.Compress, nil, nil, false, nil)
+		//l.ConnStatus = "P2P"
+		conn.CopyWaitGroup(target, localTcpConn, false, config.Client.IsCompress, nil, nil, false, nil)
+		return nil
 	}
 }
 
@@ -247,9 +293,48 @@ func newUdpConn(localAddr string, config *config.CommonConfig, l *config.LocalSe
 	p2pNetBridge = &p2pBridge{}
 }
 
+func getTunnelTypes(config *config.CommonConfig, l *config.LocalServer) (tunnelTypes string, err error) {
+	if tunneltypesTemp != "" {
+		logs.Debug("use tunnelTypes_temp : %s", tunneltypesTemp)
+		tunnelTypes = tunneltypesTemp
+		return
+	}
+	var remoteConn *conn.Conn
+	remoteConn, err = NewConn(config.Tp, config.VKey, config.Server, common.WORK_LAN, config.ProxyUrl)
+	if err != nil {
+		logs.Error("Local connection server failed ", err.Error())
+		return
+	}
+	// common.WORK_P2P 因预创建，需要对称加密
+	if err = remoteConn.WriteLenContent([]byte(common.GetAesEnVerifyval(l.Password))); err != nil {
+		logs.Error("Local connection server failed ", err.Error())
+		return
+	}
+	var rAddr []byte
+	//读取服务端地址、密钥 继续做处理
+	if rAddr, err = remoteConn.GetShortLenContent(); err != nil {
+		logs.Error(err)
+		return
+	}
+	if strings.HasPrefix(string(rAddr), "{[checked]}") {
+		err = errors.New(string(rAddr))
+		logs.Error(string(rAddr))
+		return
+	}
+	// 获得产品信息pk
+	var tunnelByte []byte
+	if tunnelByte, err = remoteConn.GetShortLenContent(); err != nil {
+		logs.Error(err)
+		return
+	}
+	tunnelTypes = string(tunnelByte)
+	tunneltypesTemp = tunnelTypes
+	return
+}
+
 func newRemoteLocalConn(localTcpConn net.Conn, localAddr string, config *config.CommonConfig, l *config.LocalServer) (err error) {
 	var remoteConn *conn.Conn
-	remoteConn, err = NewConn(config.Tp, config.VKey, config.Server, common.WORK_P2P, config.ProxyUrl)
+	remoteConn, err = NewConn(config.Tp, config.VKey, config.Server, common.WORK_LAN, config.ProxyUrl)
 	if err != nil {
 		logs.Error("Local connection server failed ", err.Error())
 		return
