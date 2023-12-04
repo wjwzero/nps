c3ad879af0246f1a9554fd60c70bb4849486fcaf
wjwzero <13602573297@163.com>
2023-12-04 09:14:15 +0800

    1. cloud.go conn.go base push

---
+++ nps_file1/lib/cloud/cloud.go
@@ -0,0 +1,108 @@
+package cloud
+
+import (
+	"ehang.io/nps/lib/crypt"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type CommonData struct {
+	Code    int                    `json:"code"`
+	Success bool                   `json:"success"`
+	Data    map[string]interface{} `json:"data"`
+	Msg     string                 `json:"msg"`
+}
+
+// GetNpsNodeExternalIp 从云平台获得节点外部IP地址
+func GetNpsNodeExternalIp(cloudAddr string, deviceKey string) (ip string, err error) {
+	var retryNum = 0
+	var retryDuration = 10
+retry:
+	ip, err = getNpsNodeExternalIp(cloudAddr, deviceKey)
+	if err != nil {
+		retryNum++
+		if retryNum < 3 {
+			retryDuration = 100 + rand.Intn(30)
+		} else if retryNum >= 3 && retryNum < 5 {
+			retryDuration = 150 + rand.Intn(30)
+		} else if retryNum >= 5 && retryNum < 10 {
+			retryDuration = 300 + rand.Intn(30)
+		} else {
+			retryDuration = 9*60 + rand.Intn(60)
+		}
+		logs.Warn(fmt.Sprintf("从云平台获得NPS节点地址失败, %d s后重试 %s", retryDuration, err.Error()))
+		time.Sleep(time.Duration(retryDuration) * time.Second)
+		goto retry
+	}
+	return
+}
+
+// getNpsNodeExternalIp 从云平台获得节点外部IP地址 client 使用
+func getNpsNodeExternalIp(cloudAddr string, deviceKey string) (ip string, err error) {
+	var data CommonData
+	resp, err := http.Get(cloudAddr + "/skyworthDispatch/node/nps/addr?deviceKey=" + deviceKey)
+	if err != nil {
+		return "", err
+	}
+	content, _ := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	err = json.Unmarshal(content, &data)
+	if data.Code != 200 {
+		return "", errors.New(fmt.Sprintf("code：%d;%s", data.Code, data.Msg))
+	}
+	ip = data.Data["addr"].(string)
+	return
+}
+
+// CheckPassword 从云平台验证password server 使用
+func CheckPassword(cloudAddr string, vKey string, password string) (res bool, err error) {
+	sign := crypt.CreateSign([]string{vKey, password})
+	var data CommonData
+	cloudAddr = beego.AppConfig.String("cloudAddr")
+	resp, err := http.Get(cloudAddr + "/skyworthUser/nas/device/judgeNasPassword?password=" + password + "&deviceKey=" + vKey + "&authString=" + sign)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+	content, _ := ioutil.ReadAll(resp.Body)
+	err = json.Unmarshal(content, &data)
+	if err != nil {
+		return false, err
+	}
+	if data.Code != 200 {
+		return false, errors.New(fmt.Sprintf("data.Code %d data.Msg %s", data.Code, data.Msg))
+	}
+	res = data.Data["judge"].(bool)
+	return
+}
+
+// CheckDeviceKey 从云平台验证vKey dk server 使用
+func CheckDeviceKey(cloudAddr string, deviceKey string) (res bool, productKey string, err error) {
+	sign := crypt.CreateSign([]string{deviceKey})
+	var data CommonData
+	resp, err := http.Get(cloudAddr + "/skyworthDevice/devices/judgeDeviceExist?deviceKey=" + deviceKey + "&authString=" + sign)
+	if err != nil {
+		return false, "", err
+	}
+	defer resp.Body.Close()
+	content, _ := ioutil.ReadAll(resp.Body)
+	err = json.Unmarshal(content, &data)
+	if err != nil {
+		return false, "", err
+	}
+	if data.Code != 200 {
+		return false, "", errors.New(fmt.Sprintf("data.Code %d data.Msg %s", data.Code, data.Msg))
+	}
+	res = data.Data["judge"].(bool)
+	if data.Data["productKey"] != nil {
+		productKey = data.Data["productKey"].(string)
+	}
+	return
+}
---
+++ nps_file1/lib/common/const.go
@@ -0,0 +1,45 @@
+package common
+
+const (
+	CONN_DATA_SEQ     = "*#*" //Separator
+	VERIFY_EER        = "vkey"
+	VERIFY_SUCCESS    = "sucs"
+	VKEY_BANED        = "bans" //vkey 被禁用
+	WORK_MAIN         = "main"
+	WORK_CHAN         = "chan"
+	WORK_CONFIG       = "conf"
+	WORK_REGISTER     = "rgst"
+	WORK_SECRET       = "sert"
+	WORK_FILE         = "file"
+	WORK_P2P          = "p2pm"
+	WORK_P2P_VISITOR  = "p2pv"
+	WORK_P2P_PROVIDER = "p2pp"
+	WORK_P2P_CONNECT  = "p2pc"
+	WORK_P2P_SUCCESS  = "p2ps"
+	WORK_P2P_END      = "p2pe"
+	WORK_P2P_LAST     = "p2pl"
+	WORK_LAN          = "lanm"
+	WORK_STATUS       = "stus"
+	RES_MSG           = "msg0"
+	RES_CLOSE         = "clse"
+	NEW_UDP_CONN      = "udpc" //p2p udp conn
+	NEW_TASK          = "task"
+	NEW_CONF          = "conf"
+	NEW_HOST          = "host"
+	CONN_TCP          = "tcp"
+	CONN_UDP          = "udp"
+	CONN_TEST         = "TST"
+	UnauthorizedBytes = `HTTP/1.1 401 Unauthorized
+Content-Type: text/plain; charset=utf-8
+WWW-Authenticate: Basic realm="easyProxy"
+
+401 Unauthorized`
+	ConnectionFailBytes = `HTTP/1.1 404 Not Found
+
+`
+	DEFULT_TUNNEL_TYPE = "LAN,P2P,Relay"
+
+	LAN_TUNNEL_TYPE   = "LAN"
+	P2P_TUNNEL_TYPE   = "P2P"
+	RELAY_TUNNEL_TYPE = "Relay"
+)
